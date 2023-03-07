52f9ef2dea4df361aaf4fd60ce52f5fb0da03bf7
cat <lumitydev@gmail.com>
2023-03-07 15:52:58 -0600

    fix sidebar spacing, add home section

--- globals/list.js
+++ globals/list.js
@@ -15,16 +15,24 @@ window.onload = function () {
         var debugContent = `<div id="bar-item-debug" class="bar-item ${out.debug.filter(x => x.id == pageId).length == 1 ? "selected" : "closed"}" onclick="toggleHidden('debug')">Debug<div class="bar-item-children">`
         for (var i = 0; i < out.debug.length; i++) {
             var entry = out.debug[i];
-            debugContent += `<a id="bar-item-debug-${entry.id}" class="bar-item-entry ${entry.id == pageId ? "selected" : ""}" style="${entry.style}" href="${entry.url}">${entry.name}</a>\n`
+            debugContent += `<a id="bar-item-debug-${entry.id}" class="bar-item-entry ${entry.id == pageId ? "selected" : ""}" style="${entry.style}" href="${entry.url}">${entry.name}</a><br />\n`
         }
         debugContent += "</div></div>"
         bar.innerHTML += debugContent;
+    }
+    if (out.home.length >= 1) {
+        var homeContent = `<div id="bar-item-home" class="bar-item ${out.home.filter(x => x.id == pageId).length == 1 ? "selected" : "closed"}" onclick="toggleHidden('home')">Home<div class="bar-item-children">`
+        for (var i = 0; i < out.home.length; i++) {
+            var entry = out.home[i];
+            homeContent += `<a id="bar-item-home-${entry.id}" class="bar-item-entry ${entry.id == pageId ? "selected" : ""}" style="${entry.style}" href="${entry.url}">${entry.name}</a><br />\n`
+        }
+        homeContent += "</div></div>"
+        bar.innerHTML += homeContent;
     }
 }
 
 var toggleHidden = function (group) {
     var group = document.getElementById(`bar-item-${group}`);
-    var group = document.getElementById(`bar-item-debug`);
     if (group.classList.contains('closed')) {
         group.classList.remove('closed')
     }
