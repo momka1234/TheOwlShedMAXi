d73d49257238e589297d316b56428bbf2d39891e
cat <lumitydev@gmail.com>
2023-04-08 00:04:46 -0500

    new homepage

--- globals/loader.js
+++ globals/loader.js
@@ -1,4 +1,23 @@
 window.onload = function () {
+    var req = new XMLHttpRequest();
+
+    req.open('GET', '/s-dat/fillers.jsonc', false);
+    req.send(null);
+    var out = JSON.parse(req.responseText);
+    console.log("DBG: fillers", out);
+
+    var elms = document.getElementsByClassName("FILLABLE_NO_STYLE");
+    for (var i = 0; i < elms.length; i++) {
+        var e = elms[i];
+        e.innerText = out[e.innerHTML];
+    }
+
+    const params = new Proxy(new URLSearchParams(window.location.search), {
+        get: (searchParams, prop) => searchParams.get(prop),
+    });
+
+    let IS_DEBUG = params.debug == 1;
+
     pageId = document.getElementById("pagedat").innerHTML.split(':')[0];
 
     var req = new XMLHttpRequest();
@@ -11,7 +30,7 @@ window.onload = function () {
     var bar = document.getElementById("sidebar-left");
     bar.innerHTML = "";
     bar.innerText = "";
-    if (out.debug.length >= 1) {
+    if (out.debug.length >= 1 && IS_DEBUG) {
         var debugContent = `<div id="bar-item-debug" class="bar-item ${out.debug.filter(x => x.id == pageId).length == 1 ? "selected" : "closed"}" onclick="toggleHidden('debug')">Debug<div class="bar-item-children">`
         for (var i = 0; i < out.debug.length; i++) {
             var entry = out.debug[i];
@@ -28,6 +47,13 @@ window.onload = function () {
         }
         homeContent += "</div></div>"
         bar.innerHTML += homeContent;
+    }
+
+    if (IS_DEBUG) {
+        bar.innerHTML += `<h3>DEBUG_INFO</h3>
+<ul>
+<li>PAGEDAT:${document.getElementById("pagedat").innerHTML}
+</ul>`;
     }
 }
 
